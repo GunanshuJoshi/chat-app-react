8f41dc2a152ee556608acea0fb55eba57f1ba16b
Gunanshu Joshi <gunanshu02@gmail.com>
2025-03-20 12:01:19 +0530

    quick fixes

--- server/src/index.js
+++ server/src/index.js
@@ -13,7 +13,9 @@ configDotenv();
 
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' })); 
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 app.use(cookieParser());
 app.use(
   cors({
